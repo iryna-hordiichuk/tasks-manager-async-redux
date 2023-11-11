01e1fce2676f4ed50a4efd741341acf366e2b9ed
Iryna Hordiichuk <72663259+IrynaHordiichuk@users.noreply.github.com>
2023-11-11 22:55:31 +0000

    deployment fix

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,6 +2,7 @@ import { StatusFilter } from "components/StatusFilter/StatusFilter";
 import { TaskCounter } from "components/TaskCounter/TaskCounter";
 import css from "./AppBar.module.css";
 
+
 export const AppBar = () => {
   return (
     <header className={css.wrapper}>
